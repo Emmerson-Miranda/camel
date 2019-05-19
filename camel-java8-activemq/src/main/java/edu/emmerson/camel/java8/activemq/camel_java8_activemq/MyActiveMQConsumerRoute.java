12355290f5261d8aecc7e88feb57aa39d2378eea
Emmerson <emmerson@emmerson.com>
2019-05-19 12:22:02 +0100

    workaroung to fix System.getProperty issue

--- camel-java8-activemq/src/main/java/edu/emmerson/camel/java8/activemq/camel_java8_activemq/MyActiveMQConsumerRoute.java
+++ camel-java8-activemq/src/main/java/edu/emmerson/camel/java8/activemq/camel_java8_activemq/MyActiveMQConsumerRoute.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.activemq.ActiveMQComponent;
+import org.apache.camel.component.properties.PropertiesComponent;
 
 /**
  * A Camel Java8 DSL Router
@@ -23,15 +24,29 @@ public class MyActiveMQConsumerRoute extends RouteBuilder {
 	}
 	
 	public void checkActiveMQComponent(String name) {
+		/*
+		for(String comp : this.getContext().getComponentNames()) {
+			System.out.println(comp);
+			System.out.println(this.getContext().getComponent(comp));
+		}
+		*/
 		
 		if(this.getContext().getComponent(name) == null) {
 			System.out.println( this.getClass().getSimpleName() + ": " + name + " component does not exist, adding a new one.");
 			
-			String connectionString = System.getProperty("activemq.connectionString");
+			PropertiesComponent pc = (PropertiesComponent) this.getContext().getComponent("properties");
+			String connectionString;
+			try {
+				connectionString = pc.parseUri("activemq.connectionString");
+				
+				System.out.println(this.getClass().getSimpleName() + ": Connection String : " + connectionString);
+				
+				this.getContext().addComponent(name, ActiveMQComponent.activeMQComponent(connectionString));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
-			System.out.println(this.getClass().getSimpleName() + ": Connection String : " + connectionString);
 			
-			this.getContext().addComponent(name, ActiveMQComponent.activeMQComponent(connectionString));
 		}else {
 			System.out.println(this.getClass().getSimpleName() + ": reusing "+ name + " component.");
 		}
--- camel-java8-activemq/src/main/java/edu/emmerson/camel/java8/activemq/camel_java8_activemq/MyActiveMQProducerRoute.java
+++ camel-java8-activemq/src/main/java/edu/emmerson/camel/java8/activemq/camel_java8_activemq/MyActiveMQProducerRoute.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.activemq.ActiveMQComponent;
+import org.apache.camel.component.properties.PropertiesComponent;
 
 /**
  * A Camel Java8 DSL Router
@@ -36,17 +37,33 @@ public class MyActiveMQProducerRoute extends RouteBuilder {
 
 	public void checkActiveMQComponent(String name) {
 		
+		/*
+		for(String comp : this.getContext().getComponentNames()) {
+			System.out.println(comp);
+			System.out.println(this.getContext().getComponent(comp));
+		}
+		*/
+		
 		if(this.getContext().getComponent(name) == null) {
 			System.out.println( this.getClass().getSimpleName() + ": " + name + " component does not exist, adding a new one.");
 			
-			String connectionString = System.getProperty("activemq.connectionString");
+			PropertiesComponent pc = (PropertiesComponent) this.getContext().getComponent("properties");
+			String connectionString;
+			try {
+				connectionString = pc.parseUri("activemq.connectionString");
+				
+				System.out.println(this.getClass().getSimpleName() + ": Connection String : " + connectionString);
+				
+				this.getContext().addComponent(name, ActiveMQComponent.activeMQComponent(connectionString));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
-			System.out.println(this.getClass().getSimpleName() + ": Connection String : " + connectionString);
 			
-			this.getContext().addComponent(name, ActiveMQComponent.activeMQComponent(connectionString));
 		}else {
 			System.out.println(this.getClass().getSimpleName() + ": reusing "+ name + " component.");
 		}
+		
 		
 	}
 
