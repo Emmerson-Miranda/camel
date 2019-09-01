4d10cef3d9bc8ef52dd6540947a3f3d49e5a2ac7
Emmerson <emmerson@emmerson.com>
2019-09-02 00:29:58 +0100

    format

--- camel-java8-vault-client/src/main/java/edu/emmerson/camel/java8/vault/camel_java8_vault_client/HashicorpVaultProcessor.java
+++ camel-java8-vault-client/src/main/java/edu/emmerson/camel/java8/vault/camel_java8_vault_client/HashicorpVaultProcessor.java
@@ -5,30 +5,25 @@ import java.util.Map;
 
 import org.apache.camel.Exchange;
 
-
+/**
+ * Transform Hashicorp response format to other format and update the body.
+ * @author Emmerson
+ *
+ */
 public class HashicorpVaultProcessor {
 
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public void processExchange(Exchange exchange) {
-    	Object b = exchange.getIn().getBody();
-    	//System.out.println("Processor -> " + b.getClass().getName()); 
-    	//System.out.println("Processor -> " + b.toString());      
-    	
-    	if(b instanceof Map){
-    		Map<String, Object> m = (Map<String, Object>) b;
-    		
-    		LinkedHashMap<String, Object> lhm = new LinkedHashMap<String, Object>();
-        	lhm.put("keyname", exchange.getProperty("keyname") );
-        	lhm.put("value", m.get("data"));
-        	exchange.getIn().setBody(lhm);
-    		/*
-    		for (Map.Entry<String, Object> entry : m.entrySet()) {
-    			Object o = entry.getValue();
-    			String oclass = o == null ? "null" : o.getClass().getName();
-    	        System.out.println(entry.getKey() + " : " + o + " : " + oclass);
-    	    }
-    		*/
-    	}
-    }
-    
+		Object b = exchange.getIn().getBody();
+
+		if (b instanceof Map) {
+			Map<String, Object> m = (Map<String, Object>) b;
+
+			LinkedHashMap<String, Object> lhm = new LinkedHashMap<String, Object>();
+			lhm.put("keyname", exchange.getProperty("keyname"));
+			lhm.put("value", m.get("data"));
+			exchange.getIn().setBody(lhm);
+		}
+	}
+
 }
--- camel-java8-vault-client/src/main/java/edu/emmerson/camel/java8/vault/camel_java8_vault_client/MainApp.java
+++ camel-java8-vault-client/src/main/java/edu/emmerson/camel/java8/vault/camel_java8_vault_client/MainApp.java
@@ -7,22 +7,21 @@ import org.apache.camel.main.Main;
  */
 public class MainApp {
 
-    /**
-     * A main() so we can easily run these routing rules in our IDE
-     */
-    public static void main(String... args) throws Exception {
-    	
-    	if(System.getProperty(Constants.PARAM_VAULT_TOKEN) == null) {
-    		System.setProperty(Constants.PARAM_VAULT_TOKEN, "s.vvMr4cJCBT8rccg6Z5DfCuZK");
-    	}
-    	
-    	if(System.getProperty(Constants.PARAM_VAULT_URL) == null) {
-    		System.setProperty(Constants.PARAM_VAULT_URL, "http://127.0.0.1:8200");
-    	}
-    	
-        Main main = new Main();
-        main.addRouteBuilder(new MyRouteBuilder());
-        main.run(args);
-    }
-}
+	/**
+	 * A main() so we can easily run these routing rules in our IDE
+	 */
+	public static void main(String... args) throws Exception {
 
+		if (System.getProperty(Constants.PARAM_VAULT_TOKEN) == null) {
+			System.setProperty(Constants.PARAM_VAULT_TOKEN, "s.vvMr4cJCBT8rccg6Z5DfCuZK");
+		}
+
+		if (System.getProperty(Constants.PARAM_VAULT_URL) == null) {
+			System.setProperty(Constants.PARAM_VAULT_URL, "http://127.0.0.1:8200");
+		}
+
+		Main main = new Main();
+		main.addRouteBuilder(new MyRouteBuilder());
+		main.run(args);
+	}
+}
